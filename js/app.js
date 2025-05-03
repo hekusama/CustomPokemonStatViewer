a5b09ce5fb5d43d67cf6d92ac46245d1f7eeb067
yangchengyu <82469422+hekusama@users.noreply.github.com>
2025-05-03 16:10:56 -0400

    Implemented script for num inputs

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { initialize } from "./modules/init.js";
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+function initApp() {
+    initialize();
+}
---
+++ js/modules/init.js
@@ -0,0 +1,23 @@
+export function initialize() {
+    initNumInput();
+}
+
+function initNumInput() {
+    const inputs = document.querySelectorAll('.num-input');
+
+    inputs.forEach(input => {
+        input.addEventListener('keydown', function(e) {
+            if (
+            e.key === "Backspace" || e.key === "Delete" || 
+            e.key === "ArrowLeft" || e.key === "ArrowRight" ||
+            e.key === "Tab"
+            ) {
+            return;
+            }
+    
+            if (!e.key.match(/^[0-9]$/)) {
+            e.preventDefault();
+            }
+        });
+    });
+}
