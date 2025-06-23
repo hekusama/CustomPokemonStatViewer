f2b7f8d14f40df2d613813b3ad30cd853b947dc1
yangchengyu <82469422+hekusama@users.noreply.github.com>
2025-06-23 01:32:14 -0400

    Load Pokemon finish

--- js/modules/init.js
+++ js/modules/init.js
@@ -1,10 +1,14 @@
-import { hpStatFormula, statFormula } from "./util.js";
+import { hpStatFormula, statFormula, fetchData, appendNewElement, toTitleCase, toDexNumber, getTypeId } from "./util.js";
 
-export function initialize() {
+export async function initialize() {
     
     initNumInput();
     initCalc();
     calcStats();
+    await loadTypes();
+    await loadAbilities();
+
+    fetchPokemon(25);
 }
 
 function initNumInput() {
@@ -86,4 +90,141 @@ function calcStats() {
     })
 
     values.total.textContent = total;
+}
+
+async function loadTypes() {
+    const types = [document.getElementById('type-primary'), 
+    document.getElementById('type-secondary')];
+
+    types.forEach(element => {
+        const emptyOption = new Option('', 0);
+        element.add(emptyOption);
+    })
+
+    let id = 1;
+    while (true) {
+        
+        const type = await fetchData(`https://pokeapi.co/api/v2/type/${id}`);
+        if (!type)
+            break;
+        types.forEach(element => {
+            const option = new Option(toTitleCase(type.name), id);
+            element.add(option);
+        })
+        id++;
+    }
+
+    types.forEach(element => {
+        const optionsArray = Array.from(element.options);
+
+        optionsArray.sort((a, b) => a.text.localeCompare(b.text));
+
+        element.innerHTML = '';
+
+        optionsArray.forEach(option => {
+            element.add(option);
+        });
+    });
+}
+
+async function loadAbilities() {
+    const abilities = [document.getElementById('ability-primary'), 
+            document.getElementById('ability-secondary'), 
+            document.getElementById('ability-hidden')];
+
+    abilities.forEach(element => {
+        const emptyOption = new Option('', 0);
+        element.add(emptyOption);
+    })
+
+    let id = 1;
+    while (true) {
+        
+        const ability = await fetchData(`https://pokeapi.co/api/v2/ability/${id}`);
+        if (!ability)
+            break;
+        abilities.forEach(element => {
+            const option = new Option(toTitleCase(ability.name), id);
+            element.add(option);
+        })
+        id++;
+    }
+
+    abilities.forEach(element => {
+        const optionsArray = Array.from(element.options);
+
+        optionsArray.sort((a, b) => a.text.localeCompare(b.text));
+
+        element.innerHTML = '';
+
+        optionsArray.forEach(option => {
+            element.add(option);
+        });
+    });
+}
+
+async function fetchPokemon(number) {
+    const pokemon = await fetchData(`https://pokeapi.co/api/v2/pokemon/${number}`)
+    console.log(pokemon);
+    
+    loadPokemon(pokemon);
+}
+
+function loadPokemon(pokemon) {
+    const fields = {
+        "dex": document.getElementById('dex-number'),
+        "types": [document.getElementById('type-primary'), 
+            document.getElementById('type-secondary')],
+        "name": document.getElementById('name-field'),
+        "image": document.getElementById('image'),
+        "abilities": [document.getElementById('ability-primary'), 
+            document.getElementById('ability-secondary'), 
+            document.getElementById('ability-hidden')],
+        "stats": [document.getElementById('hp-field'),
+            document.getElementById('att-field'),
+            document.getElementById('def-field'),
+            document.getElementById('spa-field'),
+            document.getElementById('spd-field'),
+            document.getElementById('spe-field')]
+    }
+
+    // Reset types and abilities
+    fields.types.forEach((type) => {
+        type.value = 0;
+    })
+    fields.abilities.forEach((ability) => {
+        ability.value = 0;
+    })
+
+    // Set values
+    fields.dex.value = toDexNumber(pokemon.id);
+    pokemon.types.forEach((type, index) => {
+        try {
+            const typeUrl = type.type.url;
+            fields.types[index].value = getTypeId(typeUrl);
+        }
+        catch {
+            type.value = 0;
+        }   
+    })
+    console.log(pokemon.name);
+    fields.name.value = toTitleCase(pokemon.name);
+    fields.image.src = pokemon.sprites["front_default"];
+    pokemon.abilities.forEach((ability, index) => {
+        try {
+            const abilityUrl = ability.ability.url;
+            if (ability["is_hidden"]) {
+                fields.abilities[2].value = getTypeId(abilityUrl);
+            }
+            else
+                fields.abilities[index].value = getTypeId(abilityUrl);
+        }
+        catch {
+            ability.value = 0;
+        }   
+    })
+    pokemon.stats.forEach((stat, index) => {
+        fields.stats[index].value = stat["base_stat"];
+    })
+    calcStats();
 }
--- js/modules/util.js
+++ js/modules/util.js
@@ -5,3 +5,49 @@ export function statFormula(base, nature, evs, ivs, level) {
 export function hpStatFormula(base, evs, ivs, level) {
     return parseInt((2 * base + ivs + evs / 4) * level / 100 + level + 10);
 }
+
+export async function fetchData(uri) {
+    try {
+        const response = await fetch(uri);
+
+        if(!response.ok) {
+            throw new Error(`Network Error: failed to fetch data\nError code: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export function appendNewElement(type, textContent, parent) {
+    const element = document.createElement(type);
+    element.textContent = textContent;
+    parent.appendChild(element);
+    return element;
+}
+
+export function toTitleCase(kebabCaseStr) {
+    return kebabCaseStr
+        .toLowerCase()
+        .split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+export function toKebabCase(titleCaseStr) {
+    return titleCaseStr
+        .split(' ')
+        .map(word => word.toLowerCase())
+        .join('-');
+}
+
+export function toDexNumber(number) {
+    return String(number).padStart(4, '0');
+}
+
+export function getTypeId(url) {
+    return url.split('/').slice(-2, -1)[0];
+}
