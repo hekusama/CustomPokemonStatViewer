969171454a57bf67ca42946606d4944a962b96f9
yangchengyu <82469422+hekusama@users.noreply.github.com>
2025-07-04 22:24:35 -0400

    Mobile fix test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 import { initialize } from "./modules/init.js";
+import { fix } from "./modules/mobileFix.js";
 
 document.addEventListener("DOMContentLoaded", initApp);
 
 function initApp() {
+
+    const userAgent = navigator.userAgent;
+
+    const isMobile = /Mobile|Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(userAgent);
+
+    if (isMobile) {
+        fix();
+    }
+
     initialize();
 }
