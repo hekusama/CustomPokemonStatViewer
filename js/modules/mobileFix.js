321c2953c0e40a3e366bc8d7ece6ba0671c5506a
yangchengyu <82469422+hekusama@users.noreply.github.com>
2025-07-04 22:34:39 -0400

    Mobile fix script

--- js/modules/mobileFix.js
+++ js/modules/mobileFix.js
@@ -1,3 +1,7 @@
 export function fix() {
-    
+    const dpr = window.devicePixelRatio || 1;
+
+    const baseFontSize = 16;
+
+    document.documentElement.style.fontSize = `${baseFontSize / dpr}px`;
 }
